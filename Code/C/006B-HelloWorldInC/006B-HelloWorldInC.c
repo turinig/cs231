af3b45155f7b5833632971c62d56fc386aae945d
Giuseppe Turini <turinig@gmail.com>
2024-04-02 13:46:22 -0400

    Initial commit.

---
+++ Code/C/006B-HelloWorldInC/006B-HelloWorldInC.c
@@ -0,0 +1,18 @@
+
+
+
+// 006B-HelloWorldInC.c
+// Giuseppe Turini
+// CS-231
+// 2023-12-01
+
+#include <stdio.h> // Importing external code for "printf" etc.
+
+// Entry point.
+int main() {
+	printf("Hello, World!\n");
+	fflush(stdout); // Force printf to print (otherwise is buffered).
+	return 0;
+}
+
+
