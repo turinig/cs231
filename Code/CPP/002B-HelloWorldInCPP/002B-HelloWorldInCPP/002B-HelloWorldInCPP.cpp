564374cb1e9ce199d035e4919ac0f261c6151f1f
Giuseppe Turini <turinig@gmail.com>
2024-05-06 10:27:57 -0400

    Initial commit.

---
+++ Code/CPP/002B-HelloWorldInCPP/002B-HelloWorldInCPP/002B-HelloWorldInCPP.cpp
@@ -0,0 +1,20 @@
+
+
+
+// 002B-HelloWorldInCPP.cpp
+// Giuseppe Turini
+// CS-231
+// 2024-05-05
+
+
+
+#include <iostream> // For I/O functions.
+
+// ...
+int main() {
+	std::cout << "Hello World in C++!"; // Using standard output stream.
+	fflush(stdout); // Force print on console (otherwise is buffered).
+	return 0; // Normal exit.
+}
+
+
